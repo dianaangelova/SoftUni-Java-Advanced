c5264e3ba308715ee94dd480b761959d44cd5a8c
dianaangelova <diana.anguelova@gmail.com>
2022-05-28 23:17:22 +0300

    Add files via upload

--- C3_SetsAndMapsAdvanced/E02_SetsOfElements.java
+++ C3_SetsAndMapsAdvanced/E02_SetsOfElements.java
@@ -1,15 +1,38 @@
 package C3_SetsAndMapsAdvanced;
 
-import javax.security.sasl.SaslClient;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class E02_SetsOfElements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] length = scanner.nextLine().split(" ");
+
+        int sizeFirst = Integer.parseInt(length[0]);
+        int sizeSecond = Integer.parseInt(length[1]);
+
+        Set<Integer> firstElements = new LinkedHashSet<>();
+        Set<Integer> secondElements = new LinkedHashSet<>();
+
+        for (int i = 0; i < sizeFirst; i++) {
+            firstElements.add(Integer.parseInt(scanner.nextLine()));
+        }
+        for (int i = 0; i < sizeSecond; i++) {
+            secondElements.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        Set<Integer> commonElements = new LinkedHashSet<>();
 
 
+        firstElements.retainAll(secondElements);
 
-
+        for (Integer e : firstElements) {
+            System.out.print(e);
+            System.out.print(" ");
+        }
     }
 }
+
