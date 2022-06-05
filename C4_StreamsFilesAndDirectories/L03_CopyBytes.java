2613801aa5a355e685b45bbd1cd3c455704bae53
dianaangelova <diana.anguelova@gmail.com>
2022-06-05 10:45:04 +0300

    Add files via upload

---
+++ C4_StreamsFilesAndDirectories/L03_CopyBytes.java
@@ -0,0 +1,34 @@
+package C4_StreamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class L03_CopyBytes {
+    public static void main(String[] args) throws InterruptedException {
+
+        String path = "src/04.Java-Advanced-Files-and-Streams-Lab-Resources/input.txt";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            FileOutputStream fileOutputStream = new FileOutputStream("src/04.Java-Advanced-Files-and-Streams-Lab-Resources/output.txt");
+
+            int read = (fileInputStream.read());
+            while (read != -1) {
+                byte oneByte = (byte) read;
+                String digits = String.valueOf(oneByte);
+                if (digits.equals("32")||digits.equals("10")){
+                    fileOutputStream.write(Integer.parseInt(Integer.toString(Integer.parseInt(digits))));
+                } else{
+                    for (int i = 0; i < digits.length(); i++) {
+                        fileOutputStream.write(digits.charAt(i));
+                    }
+                }
+                read = fileInputStream.read();
+            }
+
+        } catch (
+                IOException io) {
+        }
+    }
+}
