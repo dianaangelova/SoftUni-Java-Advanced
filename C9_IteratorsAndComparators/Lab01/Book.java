1269feae04ba0a07ec3c7489e5a433595b945aec
dianaangelova <diana.anguelova@gmail.com>
2022-06-16 11:21:35 +0300

    Add files via upload

---
+++ C9_IteratorsAndComparators/Lab01/Book.java
@@ -0,0 +1,33 @@
+package C9_IteratorsAndComparators.Lab01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        setAuthors();
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
