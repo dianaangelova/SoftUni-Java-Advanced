645dde3dd22299c58538656e2db4673cf0d7511a
dianaangelova <diana.anguelova@gmail.com>
2022-06-21 23:44:57 +0300

    Add files via upload
    Not ready

---
+++ ExamPreparation/Exam02_Armory.java
@@ -0,0 +1,50 @@
+package ExamPreparation;
+
+import java.util.Scanner;
+
+public class Exam02_Armory {
+    public static int rowA = 0;
+    public static int colA = 0;
+    public static int rowM1 = 0;
+    public static int rowM2 = 0;
+    public static int colM1 = 0;
+    public static int colM2 = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+        char[][] matrix = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            String[] arrayInput = scanner.nextLine().split("");
+            for (int j = 0; j < size; j++) {
+                char c = arrayInput[j].charAt(0);
+                matrix[i][j] = c;
+            }
+        }
+
+        boolean firstMirrorFound = false;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] == 'A') {
+                    rowA = i;
+                    colA = j;
+                }
+                if (matrix[i][j] == 'M') {
+                    if (!firstMirrorFound) {
+                        rowM1 = i;
+                        colM1 = j;
+                        firstMirrorFound = true;
+                    } else {
+                        rowM2 = i;
+                        colM2 = j;
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
